1f5309bfaa56ad6cced4d27000b01c3986993ca3
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-09-11 20:53:23 -0700

    Use short-hand for echo

--- c01/echo-shorthand.php
+++ c01/echo-shorthand.php
@@ -1,6 +1,6 @@
 <?php 
-$name      = 'Ivy';
-$favorites = ['Chocolate', 'Toffee', 'Fudge',];
+$name      = 'Joy';
+$favorites = ['Snickers','Chocolate', 'Toffee', 'Fudge',];
 ?>
 <!DOCTYPE html>
 <html>
